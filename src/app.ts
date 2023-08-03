15ee5279839bec63f6d612f1008474a428ad6892
Carloslt <carloslt.proyectos@gmail.com>
2023-08-03 21:00:03 +0200

    add routes/index.ts

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,15 @@ import express from 'express'
 import './db'
 import config from './config'
 import errorHandling from './error-handling'
+import routes from './routes'
 
 dotenv.config()
 
 const app = express();
+
 config(app)
 
-const indexRoutes = require("./routes/index.routes");
-app.use("/api", indexRoutes);
+app.use("/api", routes);
 
 errorHandling(app)
 
---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+import todoRoutes from "./todo.routes";
+
+const router = express.Router();
+
+router.use("/", todoRoutes)
+
+export default router;
