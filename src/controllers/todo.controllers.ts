a42474317b029b555cfd92466a02accc36e42bac
Carloslt <carloslt.proyectos@gmail.com>
2023-08-06 20:34:35 +0200

    fix todo controllers getAlltodos res.json

--- src/controllers/todo.controllers.ts
+++ src/controllers/todo.controllers.ts
@@ -4,7 +4,7 @@ import type { AsyncRequestHandler } from './Types/AsyncRequestHandler.Type'
 const getAllTodos: AsyncRequestHandler = async (req, res, next) => {
   try {
     const todos = await ToDo.find()
-    res.status(200).json({ todos })
+    res.status(200).json(todos)
   } catch (error) {
     res.status(400).json({ success: false, error })
   }
@@ -15,7 +15,7 @@ const createdTodo: AsyncRequestHandler = async (req, res, next) => {
 
   try {
     const todo = await ToDo.create({ title })
-    res.status(200).json(todo)
+    res.status(200).json({ todo })
   } catch (error) {
     res.status(400).json({ success: false, error })
   }
--- src/db/index.ts
+++ src/db/index.ts
@@ -4,7 +4,6 @@ const MONGO_URI =
   process.env.MONGODB_URI ?? 'mongodb://127.0.0.1:27017/todo-app'
 
 mongoose.connect(MONGO_URI)
-
   .then((x) => {
     const dbName = x.connections[0].name
     console.log(`Connected to Mongo! Database name: "${dbName}"`)
