c908586a9cd6e195eb82bbe35cae6d24d9821c03
Carloslt <carloslt.proyectos@gmail.com>
2023-08-21 13:39:58 +0200

    update kanbancontroller, add order +1

--- src/controllers/kanban.controllers.ts
+++ src/controllers/kanban.controllers.ts
@@ -3,10 +3,9 @@ import { type AsyncRequestHandler } from './Types/AsyncRequestHandler.Type'
 
 const getKanbanCards: AsyncRequestHandler = async (req, res, next) => {
   const { boardId } = req.params
-
   try {
     const kanbanCards = await KanbanCard.find({ boardId }).sort('order')
-    res.status(200).json({ kanbanCards })
+    res.status(200).json(kanbanCards)
   } catch (error) {
     res.status(500).json({ success: false, error })
   }
@@ -14,11 +13,12 @@ const getKanbanCards: AsyncRequestHandler = async (req, res, next) => {
 
 const createKanbanCards: AsyncRequestHandler = async (req, res, next) => {
   const { boardId } = req.params
-  const { title, order } = req.body
+  const { title } = req.body
 
   try {
-    const kanbanCard = await KanbanCard.create({ boardId, title, order })
-    res.status(200).json({ kanbanCard })
+    const newOrder = ((await KanbanCard.find({ boardId })).length + 1)
+    const kanbanCard = await KanbanCard.create({ boardId, title, order: newOrder })
+    res.status(200).json(kanbanCard)
   } catch (error) {
     res.status(500).json({ success: false, error })
   }
