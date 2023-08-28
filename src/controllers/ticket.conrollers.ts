2f6fd329bc3642db5045af0eb07d8aed3d30e3e6
Carloslt <carloslt.proyectos@gmail.com>
2023-08-28 20:07:55 +0200

    ticket endpoint

--- src/controllers/ticket.conrollers.ts
+++ src/controllers/ticket.conrollers.ts
@@ -1,10 +1,19 @@
 import Ticket from '../models/Ticket.model'
 import { type AsyncRequestHandler } from './Types/AsyncRequestHandler.Type'
 
+const getTicket: AsyncRequestHandler = async (req, res, next) => {
+  try {
+    const tickets = await Ticket.find().populate('state')
+    res.status(200).json(tickets)
+  } catch (error) {
+    res.status(500).json({ success: false, error })
+  }
+}
+
 const createdTicket: AsyncRequestHandler = async (req, res, next) => {
   const { _id } = req.payload
   const { projectId } = req.params
-  const { title, satateId } = req.body
+  const { state: { _id: satateId }, newTicket: { title } } = req.body
 
   try {
     const createdTicket = await Ticket.create({ title, projectId, state: satateId, owner: _id })
@@ -15,5 +24,6 @@ const createdTicket: AsyncRequestHandler = async (req, res, next) => {
 }
 
 export {
+  getTicket,
   createdTicket
 }
--- src/routes/ticket.routes.ts
+++ src/routes/ticket.routes.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import { isAuthenticated } from '../middlewares/verifyToken.middleware'
-import { createdTicket } from '../controllers/ticket.conrollers'
+import { getTicket, createdTicket } from '../controllers/ticket.conrollers'
 
 const router = express.Router()
 
+router.get('/getTicket/:projectId', isAuthenticated, getTicket)
 router.post('/createdTicket/:projectId', isAuthenticated, createdTicket)
 
 export default router
