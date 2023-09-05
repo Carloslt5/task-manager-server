3c4815a38b12d3909fe7fd9aecbfe2e950e786a3
Carloslt <carloslt.proyectos@gmail.com>
2023-09-05 11:26:25 +0200

    verifytoken

--- src/middlewares/verifyToken.middleware.ts
+++ src/middlewares/verifyToken.middleware.ts
@@ -1,19 +1,20 @@
 import { type Request } from 'express'
-import { expressjwt } from 'express-jwt'
+import { type TokenGetter, expressjwt } from 'express-jwt'
 import { type Secret } from 'jsonwebtoken'
 
 const isAuthenticated = expressjwt({
   secret: process.env.TOKEN_SECRET as Secret,
   algorithms: ['HS256'],
   requestProperty: 'payload',
-  getToken: getTokenFromHeaders
+  getToken: getTokenFromHeaders as TokenGetter
 })
 
-function getTokenFromHeaders(req: Request): string | undefined {
+function getTokenFromHeaders(req: Request): string | undefined | null {
   if (req?.headers?.authorization?.split(' ')[0] === 'Bearer') {
     const token = req.headers.authorization.split(' ')[1]
     return token
   }
+  return null
 }
 
 export { isAuthenticated }
