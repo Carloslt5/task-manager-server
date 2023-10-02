1ffb104872e7206d7793dcb08c379b16a27765d2
Carloslt <carloslt.proyectos@gmail.com>
2023-10-02 17:36:50 +0200

    add description and priority on ticket

--- src/controllers/ticket.conrollers.ts
+++ src/controllers/ticket.conrollers.ts
@@ -14,10 +14,10 @@ const getTicket: AsyncRequestHandler = async (req, res, next) => {
 const createdTicket: AsyncRequestHandler<UserPayload> = async (req, res, next) => {
   const _id = req.payload?._id
   const { projectId } = req.params
-  const { stateId, newTicket: { title } } = req.body
+  const { stateId, newTicket: { title, description } } = req.body
 
   try {
-    const createdTicket = await Ticket.create({ title, project: projectId, state: stateId, owner: _id })
+    const createdTicket = await Ticket.create({ title, description, project: projectId, state: stateId, owner: _id })
     res.status(200).json(createdTicket)
   } catch (error) {
     res.status(500).json({ success: false, error })
