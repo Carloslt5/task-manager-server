0710b2b451326e11cc1f8e809f72cf33cc9530d4
Carloslt <carloslt.proyectos@gmail.com>
2023-10-26 18:59:13 +0200

    createProjectSchema

--- src/schemas/project.schema.ts
+++ src/schemas/project.schema.ts
@@ -18,5 +18,13 @@ export const updateProjectSchema = z.object({
   })
 })
 
+export const createProjectSchema = z.object({
+  body: z.object({
+    title: z.string().min(5, 'Title requires a minimum of 5 characters').trim(),
+    description: z.string().min(15, 'Description requires a minimum of 15 characters').trim()
+  })
+
+})
+
 export type ProjectParamsType = z.infer<typeof projectSchema>['params']
 export type ProjectBodyType = z.infer<typeof projectSchema>['body']
