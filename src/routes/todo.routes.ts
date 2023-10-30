c544193468013fd7eeb48172acc4c424321c3f30
Carloslt <carloslt.proyectos@gmail.com>
2023-10-30 13:30:02 +0100

    add create todo schema validation

--- src/routes/todo.routes.ts
+++ src/routes/todo.routes.ts
@@ -2,12 +2,14 @@ import express from 'express'
 import { isAuthenticated } from '../middlewares/verifyToken.middleware'
 import { checkUserOwner } from '../middlewares/checkUserOwner'
 import { getAllTodos, createdTodo, updateTodo, updateTitleToDo, deleteTodo, deleteCompletedTodos, updateTodoOrder, getTicketToDos } from '../controllers/todo.controllers'
+import { schemaValidation } from '../middlewares/schemaValidation'
+import { createToDoSchema } from '../schemas/todo.schema'
 
 const router = express.Router()
 
 router.get('/:id/getAllTodos', isAuthenticated, checkUserOwner, getAllTodos)
 router.get('/:id/:ticketID/getTicketToDos', isAuthenticated, checkUserOwner, getTicketToDos)
-router.post('/:id/createdTodo', isAuthenticated, checkUserOwner, createdTodo)
+router.post('/:id/createdTodo', schemaValidation(createToDoSchema), isAuthenticated, checkUserOwner, createdTodo)
 router.put('/:id/updateTodo', isAuthenticated, checkUserOwner, updateTodo)
 router.put('/:id/updateTitleToDo', isAuthenticated, checkUserOwner, updateTitleToDo)
 router.put('/:id/updateTodoOrder', isAuthenticated, checkUserOwner, updateTodoOrder)
