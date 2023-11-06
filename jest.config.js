aaa956eb07ad5125a2b4b742faeb395774d50a05
Anthanh <antai.pt@gmail.com>
2023-11-06 23:28:58 +0100

    test: initial jest integration

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+const swcConfig = JSON.parse(fs.readFileSync(`${__dirname}/.swcrc`, "utf-8"));
+
+// Jest configuration
+// https://jestjs.io/docs/en/configuration
+/** @type {import('jest').Config} */
+const config = {
+  clearMocks: true,
+
+  // Test all files either suffixed with "-test.js", "-test.jsx", "-test.ts", "-test.tsx", or
+  // having ".test.js", ".test.jsx", ".test.ts", ".test.tsx" extensions
+  testRegex: ".*[-.]test\\.(js|ts)x?$",
+
+  setupFiles: [
+    // Configure environment variables for the test environment
+    "<rootDir>/jest.setup.js",
+  ],
+
+  transform: {
+    "^.+\\.(t|j)sx?$": ["@swc/jest", { ...swcConfig }],
+  },
+  // File extensions to be tested
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+};
+
+module.exports = config;
---
+++ jest.setup.js
@@ -0,0 +1,6 @@
+// Force Node env to test
+process.env.NODE_ENV = "test";
+
+process.env.TZ = "Europe/Madrid";
+
+process.env.TOKEN_SECRET = 'secret'
