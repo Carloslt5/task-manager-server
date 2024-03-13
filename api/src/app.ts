4bcf3d377a7b17c6d9375020f8645a550bc10847
Carloslt <carloslt.proyectos@gmail.com>
2024-03-13 14:20:40 +0100

    ✅ feature: add pg and connect with postgres db

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,18 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
 import config from './config';
+import './db';
 import errorHandling from './error-handling';
 import routes from './routes';
-import { connect } from './db';
-import helmet from 'helmet';
-
-connect();
 
 const app = express();
-
 config(app);
 
-app.use(helmet());
 app.use('/api', routes);
 
 errorHandling(app);
