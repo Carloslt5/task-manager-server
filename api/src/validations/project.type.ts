463630512293e429ae7b075ec4a61d4489aeb35d
Carloslt <carloslt.proyectos@gmail.com>
2024-04-26 22:11:55 +0200

    🗑 delete: delete old projoect schema

--- api/src/validations/project.type.ts
+++ api/src/validations/project.type.ts
@@ -3,7 +3,6 @@ import { z } from 'zod';
 const projectProperties = {
   title: z.string().min(5, 'Title requires a minimum of 5 characters').trim(),
   description: z.string().min(15, 'Description requires a minimum of 15 characters').trim(),
-  ownerID: z.string(),
 };
 
 export const createProjectSchema = z.object({
